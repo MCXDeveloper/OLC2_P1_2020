5a4807c6864ec946741dbbca0fc9d0c335b8e139
MCXDeveloper <2327239740101@ingenieria.usac.edu.gt>
2020-02-21 15:16:06 -0600

    Se modifico la estructura de la modificacion de vectores.

--- src/com/arbol/NAsiArr.java
+++ src/com/arbol/NAsiArr.java
@@ -88,85 +88,93 @@ public class NAsiArr extends Nodo implements Instruccion {
             msj = "Error. No se puede acceder a un vector utilizando la forma de acceso proporcionada.";
             ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
             return false;
+        }
+
+        /*
+         * Una vez validado lo anterior, se procede a verificar que el valor proporcionado
+         * como posición de la primera dimensión sea de tipo entero y que sea mayor a 0.
+        */
+
+        int actualPos = 0;
+        Resultado rdim = ((Instruccion)listaDims.get(0).getValorDimIzq()).Ejecutar(ts);
+
+        if (rdim.getTipoDato() != ETipoDato.INT) {
+            msj = "Error. Se espera que el valor propocionado como posición en la dimension 1 sea de tipo INTEGER.";
+            ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
+            return false;
+        }
+
+        int firstPos = (int)rdim.getValor();
+
+        if (firstPos <= 0) {
+            msj = "Error. El indice proporcionado debe ser mayor o igual a 1.";
+            ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
+            return false;
+        }
+
+        /*
+         * Una vez validado lo anterior, se procede a verificar si la lista de dimensiones
+         * es de una sola dimensión o de varias ya que el procedimiento es distinto.
+        */
+
+        if (listaDims.size() == 1) {
+            actualPos = firstPos;
         } else {
 
-            int actualPos = 0;
-            Resultado rdim = ((Instruccion)listaDims.get(0).getValorDimIzq()).Ejecutar(ts);
+            /*
+             * Verifico si el acceso al vector posee más de una dimension. Si eso se cumple,
+             * se tiene que validar que las siguientes dimensiones proporcionen un entero como
+             * valor de posicion y este tiene que ser exactamente igual que el primero, de lo
+             * contrario se debe reportar error.  Éste multiple acceso simula la recursión interna
+             * de la relación entre vectores y tipos primitivos (que también se consideran vectores).
+             *
+             * Una vez finalizado el ciclo y si todas las dimensiones se validaron correctamente
+             * significa que todas las posiciones son iguales, por lo que se procede a tomar la
+             * última (actualPos) y se modifica su valor por la expresión proporcionada. Esto se
+             * realiza más abajo.
+             */
 
-            if (rdim.getTipoDato() != ETipoDato.INT) {
-                msj = "Error. Se espera que el valor propocionado como posición en la dimension 1 sea de tipo INTEGER.";
-                ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
-                return false;
-            } else {
-
-                int firstPos = (int)rdim.getValor();
-
-                if (firstPos <= 0) {
-                    msj = "Error. El indice proporcionado debe ser mayor o igual a 1.";
+            Resultado r;
+            for (int i = 1; i < listaDims.size(); i++) {
+                r = ((Instruccion)listaDims.get(i).getValorDimIzq()).Ejecutar(ts);
+                if (r.getTipoDato() != ETipoDato.INT) {
+                    msj = "Error. Se espera que el valor propocionado como posición en la dimension "+ (i+1) +" sea de tipo INTEGER.";
                     ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
                     return false;
-                }
-
-                if (listaDims.size() == 1) {
-                    actualPos = firstPos;
                 } else {
-
-                    /*
-                     * Verifico si el acceso al vector posee más de una dimension. Si eso se cumple,
-                     * se tiene que validar que las siguientes dimensiones proporcionen un entero como
-                     * valor de posicion y este tiene que ser exactamente igual que el primero, de lo
-                     * contrario se debe reportar error.  Éste multiple acceso simula la recursión interna
-                     * de la relación entre vectores y tipos primitivos (que también se consideran vectores).
-                     */
-
-                    Resultado r;
-                    for (int i = 1; i < listaDims.size(); i++) {
-                        r = ((Instruccion)listaDims.get(i).getValorDimIzq()).Ejecutar(ts);
-                        if (r.getTipoDato() != ETipoDato.INT) {
-                            msj = "Error. Se espera que el valor propocionado como posición en la dimension "+ (i+1) +" sea de tipo INTEGER.";
-                            ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
-                            return false;
-                        } else {
-                            actualPos = (int)r.getValor();
-                            if (actualPos != firstPos) {
-                                msj = "Error. No se puede acceder a una posición inexistente de un vector [Dimension = "+ (i+1) +" | Valor = "+ actualPos +"].";
-                                ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
-                                return false;
-                            }
-                        }
+                    actualPos = (int)r.getValor();
+                    if (actualPos != firstPos) {
+                        msj = "Error. No se puede acceder a una posición inexistente de un vector [Dimension = "+ (i+1) +" | Valor = "+ actualPos +"].";
+                        ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
+                        return false;
                     }
-
-                    /*
-                     * Una vez finalizado el ciclo de arriba y llegado a este punto significa que todas las
-                     * posiciones son iguales, por lo que se procede a tomar la última (actualPos) y se
-                     * modifica su valor por la expresión proporcionada. Esto se realiza más abajo.
-                     */
-
-                }
-
-            }
-
-            Resultado rexp = ((Instruccion)valor).Ejecutar(ts);
-            if (rexp.getTipoDato() == ETipoDato.ARRAY || rexp.getTipoDato() == ETipoDato.MATRIX) {
-                msj = "Error. No se puede asignar un valor de tipo <"+ rexp.getTipoDato() +"> a una posición de un vector.";
-                ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
-                return false;
-            } else {
-                /* Actualizo el valor del arreglo. */
-                if (!((Vector)s.getValor()).updateVectorValue(actualPos, rexp.getTipoDato(), rexp.getValor())) {
-                    msj = "Error. No se pudo actualizar/castear los valores del vector.";
-                    ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
-                    return false;
                 }
             }
+        }
 
+        /* Ejecuto la expresión para obtener su valor y validar que ésta sea de un tipo permitido. */
+
+        Resultado rexp = ((Instruccion)valor).Ejecutar(ts);
+
+        if (rexp.getTipoDato() == ETipoDato.ARRAY || rexp.getTipoDato() == ETipoDato.MATRIX) {
+            msj = "Error. No se puede asignar un valor de tipo <"+ rexp.getTipoDato() +"> a una posición de un vector.";
+            ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
+            return false;
+        }
+
+        /* Actualizo el valor del arreglo. */
+        if (!((Vector)s.getValor()).updateVectorValue(actualPos, rexp.getTipoDato(), rexp.getValor())) {
+            msj = "Error. No se pudo actualizar/castear los valores del vector.";
+            ErrorHandler.AddError(getTipoError(), getArchivo(), "[N_ASI_ARR]", msj, getLinea(), getColumna());
+            return false;
         }
 
         return true;
+
     }
 
     private boolean validarActualizarLista() {
-        return false;
+        return true;
     }
 
 }
